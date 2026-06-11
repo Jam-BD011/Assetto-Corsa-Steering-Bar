caa8a1a110795c34fcd11ac322be92b5ba0a1768
Jam-BD011 <259469060+Jam-BD011@users.noreply.github.com>
2026-06-11 17:02:29 -0600

    GNU License notice

--- SteerBar.py
+++ SteerBar.py
@@ -4,8 +4,23 @@ import math
 import os
 
 #Author: Jam-BD011
-#MIT License
-#Assetto Corsa Steering Bar App
+"""
+ Assetto Corsa Steering Bar; a steering visualization app for Assetto Corsa
+    Copyright (C) 2026 Jam-BD011
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
 
 # =========================================================
 # APPS
